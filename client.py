2d69f91305174553ac510c6b512998e29eba21d3
DJEBARRI Amrane <adjebarri@esiea.fr>
2022-03-21 15:41:43 +0100

    mon fichier client

--- client.py
+++ client.py
@@ -20,6 +20,15 @@ if __name__ == "__main__":
         print(res)
 
 
+if __name__ == "__main__":
+
+    main = Main()
+    hostname = "127.0.0.1:8000"
+    res = main.query(hostname)
+    if res:
+        print(res)
+
+
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
@@ -42,13 +51,26 @@ class MainWindow(QWidget):
     def initUI(self):
         self.setWindowTitle("Client")
         self.setFixedSize(400, 400)
+
         self.label1 = QLabel("Enter your host IP:", self)
         self.text = QLineEdit(self)
         self.text.move(10, 30)
-        self.label2 = QLabel("Answer:", self)
-        self.label2.move(10, 60)
+       
         self.button = QPushButton("Send", self)
-        self.button.move(10, 90)
+        self.button.move(10, 190)
+        #ici_debut*****************************************************************
+        self.label3 = QLabel("Enter your keyAPI:", self)
+        self.label3.move(10, 60)
+        self.text3 = QLineEdit(self)
+        self.text3 = self.text3.move(10, 90)
+        
+
+        self.label4 = QLabel("hostname:", self)
+        self.label4.move(10, 120)
+        self.text4 = QLineEdit(self)
+        self.text4 = self.text4.move(10, 150)
+       
+        #ici_fin**********************************************************************
 
         self.button.clicked.connect(self.on_click)
         self.button.pressed.connect(self.on_click)
@@ -57,18 +79,22 @@ class MainWindow(QWidget):
 
     def on_click(self):
         hostname = self.text.text()
+        #ici_debut***********************************************************************
+        api_key = self.text3.text()
+        ip = self.text4.text()
+        #ici_fin**************************************************************************
 
-        if hostname == "":
+        if hostname == "" or api_key == "" or ip == "": # j'ai modifié a partir de "" de hostname
             QMessageBox.about(self, "Error", "Please fill the field")
         else:
-            res = self.__query(hostname)
+            res = self.__query(hostname, ip, api_key) # j'ai donné les parametre que j'ai rajouté dans la fonction
             if res:
                 self.label2.setText("Answer%s" % (res["Hello"]))
                 self.label2.adjustSize()
                 self.show()
 
-    def __query(self, hostname):
-        url = "http://%s" % (hostname)
+    def __query(self, hostname, ip, api_key):# j'ai rajouté mes 2 autres String
+        url = "http://%s/ip/%s?key=%s" % (hostname) % (ip) % (api_key)  
         r = requests.get(url)
         if r.status_code == requests.codes.NOT_FOUND:
             QMessageBox.about(self, "Error", "IP not found")
