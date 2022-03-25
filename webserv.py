c383af21f8a659ff882841fcce4a3c9d8e5f37d2
DJEBARRI Amrane <adjebarri@esiea.fr>
2022-03-25 14:27:14 +0100

    ma version finale de webserv

--- webserv.py
+++ webserv.py
@@ -1,13 +1,10 @@
-from typing import Optional
 from fastapi import FastAPI
+from typing import Optional
+from shodan import Shodan
+from colorama import init
+
 
 app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-from shodan import Shodan
 
 @app.get("/ip/{ip}")
 async def get_ip(ip: str, key: Optional[str] = None):
@@ -18,9 +15,22 @@ async def get_ip(ip: str, key: Optional[str] = None):
             api = Shodan(key)
             res = api.host(ip)
             return {
-                "IP": res["ip_str"],
-                "Organization": res["org"],
-                "Country": res["country_name"],
+                "long": res["longitude"],
+                "lat": res["latitude"],
             }
         except Exception as e:
             return {"Error": str(e)}
+
+async def read_root():
+    return [{"Hello": "World"},
+           {"Hello2": "World2"},
+           {"Hello2": "World2"}]
+
+def get_items():
+    {"id":1,"description": "Item 1"}
+
+if __name__ == '__main__':
+    init()
+    uvicorn.run(app,host="127.0.0.1", port=8000)
+
+#lancement du werver uvicorn webserv:app --reload
