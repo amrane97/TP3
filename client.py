9a8a51594caa2450e6cbe6a4c20615e3842f2c82
DJEBARRI Amrane <adjebarri@esiea.fr>
2022-03-25 14:27:57 +0100

    mon client final

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
-
-
+import webbrowser
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
@@ -21,27 +20,29 @@ class MainWindow(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Client")
-        self.setFixedSize(400, 400)
-
-        self.label1 = QLabel("Enter your host IP:", self)
-        self.text = QLineEdit(self)
-        self.text.move(10, 30)
-       
-        self.button = QPushButton("Send", self)
-        self.button.move(10, 190)
-        #ici_debut*****************************************************************
-        self.label3 = QLabel("Enter your keyAPI:", self)
-        self.label3.move(10, 60)
-        self.text3 = QLineEdit(self)
-        self.text3 = self.text3.move(10, 90)
+        self.setFixedSize(400, 600)
         
+        #Isertion host ip
+        self.label1 = QLabel("Enter your host IP:", self)
+        self.label1.move(20, 30)
+        self.text1 = QLineEdit(self)
+        self.text1.move(20, 50)
 
-        self.label4 = QLabel("hostname:", self)
-        self.label4.move(10, 120)
+       #Isertion Insertion Api key
+        self.label3 = QLabel("Enter your api_key:", self)
+        self.label3.move(20, 100)
+        self.text3 = QLineEdit(self)
+        self.text3.move(20, 120)
+
+        #Isertion Insertion Hostname
+        self.label4 = QLabel("Enter your Hostname:", self)
+        self.label4.move(20, 160)
         self.text4 = QLineEdit(self)
-        self.text4 = self.text4.move(10, 150)
-       
-        #ici_fin**********************************************************************
+        self.text4.move(20, 180)
+
+        self.button = QPushButton("Send", self)
+        self.button.move(10, 220)
+
 
         self.button.clicked.connect(self.on_click)
         self.button.pressed.connect(self.on_click)
@@ -49,31 +50,43 @@ class MainWindow(QWidget):
         self.show()
 
     def on_click(self):
-        hostname = self.text.text()
-        #ici_debut***********************************************************************
-        api_key = self.text3.text()
-        ip = self.text4.text()
-        #ici_fin**************************************************************************
+        hostname = self.text4.text()
+        hostIp = self.text1.text()
+        apiKey = self.text3.text()
 
-        if hostname == "" or api_key == "" or ip == "": # j'ai modifié a partir de "" de hostname
+        if hostname == "" or hostIp == "" or apiKey =="":
             QMessageBox.about(self, "Error", "Please fill the field")
         else:
-            res = self.__query(hostname, ip, api_key) # j'ai donné les parametre que j'ai rajouté dans la fonction
+            res = self.__query(hostname,hostIp,apiKey)
+            print(res)
             if res:
-                self.label2.setText("Answer%s" % (res["Hello"]))
-                self.label2.adjustSize()
+                self.__processing(hostname,hostIp,apiKey)
+                self.label4.setText("Answer%s" % (res["Hello"]))
+                self.label4.adjustSize()
                 self.show()
-
-    def __query(self, hostname, ip, api_key):# j'ai rajouté mes 2 autres String
-        url = "http://%s/ip/%s?key=%s" % (hostname) % (ip) % (api_key)  
+            
+    def __query(self, hostname,hostIp,apiKey):
+        url = "http://%s/ip/%s?key=%s" % (hostname,hostIp,apiKey)
+        print(url)
         r = requests.get(url)
         if r.status_code == requests.codes.NOT_FOUND:
             QMessageBox.about(self, "Error", "IP not found")
         if r.status_code == requests.codes.OK:
             return r.json()
+    
+    def __processing(self, hostname,hostIp,apiKey):
+        lat = self.__query(hostname,hostIp,apiKey)["lat"]
+        long = self.__query(hostname,hostIp,apiKey)["long"]
+        print(long,lat)
+
+        url = "https://www.openstreetmap.org/?mlat=%s&mlon=%s#map=12" % (lat,long)
+        webbrowser.open_new(url)
+        
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main = MainWindow()
     app.exec_()
+
+#interface graphique 6
